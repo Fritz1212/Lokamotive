b43d871dfdb33dbfa7cdb9d989946da3b3433cbc
Fritz Gradiyoga <slavs223@gmail.com>
2025-03-04 15:35:25 +0700

    benerin yang udah terlanjut
    pindahin routePage dari main

--- lib/routePage.dart
+++ lib/routePage.dart
@@ -1,258 +1,199 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:lokamotive/expandaleCard.dart';
+import 'package:lokamotive/mapPage.dart';
 
-class MapPage extends StatelessWidget {
+class RoutePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Stack(
-        children: [
-          // Peta
-          Positioned.fill(
-            child: Image.network(
-              'https://tile.openstreetmap.org/10/567/389.png', // Placeholder untuk OpenStreetMap
-              fit: BoxFit.cover,
-            ),
-          ),
+        backgroundColor: Colors.white,
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   leading: Icon(CupertinoIcons.home),
+        //   title: Text("LokaMotive"),
+        // ),
 
-          // Tombol Back
-          Positioned(
-            top: 40,
-            left: 20,
-            child: FloatingActionButton(
-              mini: true,
-              backgroundColor: Colors.white,
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Icon(Icons.arrow_back, color: Colors.black),
-            ),
-          ),
-
-          // Tombol GPS
-          Positioned(
-            top: 40,
-            right: 20,
-            child: FloatingActionButton(
-              mini: true,
-              backgroundColor: Colors.white,
-              onPressed: () {},
-              child: const Icon(Icons.gps_fixed, color: Colors.black),
-            ),
-          ),
-
-          // Informasi Lokasi
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Current Location",
-                    style: TextStyle(
-                      color: Colors.orange,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "Rumah Talenta BCA",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+        body: Container(
+          height: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 385 / 917 * screenHeight,
+                child: Stack(
+                  clipBehavior: Clip.none, // mencegah widget biar ga kepotong
+                  children: [
+                    // Center(
+                    //   child: ElevatedButton(
+                    //     onPressed: () {
+                    //       Navigator.pushNamed(context, '/second');
+                    //     },
+                    //     child: Text("Go to Second Page"),
+                    //   ),
+                    // ),
+                    Container(
+                      width: 413 / 412 * screenWidth,
+                      height: 364 / 917 * screenHeight,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(60),
+                          bottomRight: Radius.circular(60),
+                          topLeft: Radius.zero,
+                          topRight: Radius.zero,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage('assets/test.png'),
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.orange,
-                          backgroundColor: Colors.white,
-                          side: const BorderSide(color: Colors.orange),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    Positioned(
+                      top: 39 / 917 * screenHeight,
+                      left: 27 / 412 * screenWidth,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF102E48),
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          icon: Image.asset('assets/iconspng/backRutePage.png',
+                              width: 21, height: 19),
+                          // const Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                          onPressed: () {
+                            // Kembali ke halaman sebelumnya
+                          },
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 206.4 / 917 * screenHeight,
+                      left: 30 / 412 * screenWidth,
+                      child: Container(
+                        width: 351 / 412 * screenWidth,
+                        height: 217 / 917 * screenHeight,
+                        padding: const EdgeInsets.all(18),
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF225477),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
                           ),
                         ),
-                        child: const Text("edit"),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 5),
-                  const Text(
-                    "Jl. Pakuan No.3, Sumur Batu, Kec. Babakan Madang, Kabupaten Bogor, Jawa Barat 16810",
-                    style: TextStyle(fontSize: 12, color: Colors.black54),
-                  ),
-                  const Divider(),
-                  const SizedBox(height: 5),
-                  const Text(
-                    "Nearest public transportation",
-                    style: TextStyle(fontSize: 14, color: Colors.black),
-                  ),
-                  const SizedBox(height: 15),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF1F497D),
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+
+                        // BUAT INTERACTIVE MAP (BISA DIPENCET) & BUAT SEARCH BAR
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                  context: context,
+
+                                  builder: (context) =>
+                                      MapPage(), // Ganti dengan halaman tujuan
+                                );
+                              },
+                              child: Container(
+                                width: 287 / 412 * screenWidth,
+                                height: 122 / 917 * screenHeight,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                            ),
+
+                            // const SizedBox(height: 16),
+                            Container(
+                              margin:
+                                  EdgeInsets.only(top: 16 / 917 * screenHeight),
+                              width: 287 / 412 * screenWidth,
+                              height: 38 / 917 * screenHeight,
+                              child: TextField(
+                                style: TextStyle(color: Colors.black),
+                                textAlign: TextAlign.center,
+                                decoration: InputDecoration(
+                                  hintText: "Search Location",
+                                  hintStyle: TextStyle(
+                                    color: Colors.black.withOpacity(0.7),
+                                    fontSize: 13,
+                                  ),
+                                  prefixIcon: Padding(
+                                    padding: EdgeInsets.all(0),
+                                    child: Image.asset(
+                                      'assets/iconspng/locationIconRutePage.png',
+                                      width: 27,
+                                      height: 27,
+                                    ),
+                                  ),
+                                  suffixIcon: Padding(
+                                    padding: EdgeInsets.all(0),
+                                    child: Image.asset(
+                                      'assets/iconspng/searchIconRutePage.png',
+                                      width: 27,
+                                      height: 27,
+                                    ),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      child: const Text(
-                        "Next",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+              Container(
+                width: 351 / 412 * screenWidth,
+                height: 380 / 917 * screenHeight,
+                decoration: const BoxDecoration(
+                    color: Color(0xFF225477),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    )),
+                margin: EdgeInsets.only(top: 70 / 917 * screenHeight),
+                padding: EdgeInsets.only(top: 20 / 917 * screenHeight),
+                child: SingleChildScrollView(
+                    child: Column(children: [
+                  ExpandableCardUwi(
+                    nama: "Rumah Pak BUDI",
+                    alamat: "Jl Pakuan No.3",
+                    pintuMasuk1: "Gerbang Masuk",
+                    pintuMasuk2: "Lobby Utama",
+                  ),
+                  SizedBox(height: 10 / 917 * screenHeight),
+                  ExpandableCardUwi(
+                      nama: "Rumah Pak Yoga", alamat: "Jl Sawi No.3"),
+                  SizedBox(height: 10 / 917 * screenHeight),
+                  ExpandableCardUwi(
+                      nama: "Rumah Pak Tofer", alamat: "Jl Paku No.3"),
+                  SizedBox(height: 10 / 917 * screenHeight),
+                  ExpandableCardUwi(
+                      nama: "Rumah Pak Iyan", alamat: "Jl Palu No.3"),
+                  SizedBox(height: 10 / 917 * screenHeight),
+                  ExpandableCardUwi(
+                      nama: "Rumah Pak Yoga", alamat: "Jl Sawi No.3"),
+                  SizedBox(height: 10 / 917 * screenHeight),
+                ])),
+              )
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a purple toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
